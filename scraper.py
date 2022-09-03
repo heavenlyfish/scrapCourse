3367486ba0ca56473d12f099ea1b91a2c1da4275
heavenlyfish <42778096+heavenlyfish@users.noreply.github.com>
2022-09-03 16:57:42 +0800

    3.3 靜態網頁擷取 跟新

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.bbc.com/news')
+response = requests.get('https://www.bbc.com/news/world')
 soup = BeautifulSoup(response.text, 'lxml')
-title = soup.find('a',{'class':'bbc-uk8dsi e1d658bg0'})
-print(title)
+titles = soup.find_all('a',{'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'})
+
+title_list =[]
+for title in titles:
+    title_list.append(title.getText())
+
+print(title_list)
