15f911d99ac3dc5b0ad82aeb63e9e65e8744ba3e
heavenlyfish <42778096+heavenlyfish@users.noreply.github.com>
2022-09-03 18:37:04 +0800

    3.6 提升網頁爬蟲效率的非同步技巧

--- scraper.py
+++ scraper.py
@@ -1,25 +1,39 @@
-import requests
+import grequests
 from bs4 import BeautifulSoup
+import time
 
-for page in range(1,4):
-    response = requests.get(
-        f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt?page={page}')
-    soup = BeautifulSoup(response.text, 'lxml')
+start_time = time.time()
+
+links = [ f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt?page={page}' for page in range(1,4)]
+ 
+reqs = (grequests.get(link) for link in links)
+resps = grequests.imap(reqs, grequests.Pool(3))
+for index, resp in enumerate(resps):
+    
+    soup = BeautifulSoup(resp.text, 'lxml')
     titles = soup.find_all('h2',{'class':'bbc-19hmebw e47bds20'})
 
     title_list=[]
     for title in titles:
         title_list.append(title.getText())
+
     urls = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
-    for url in urls:
-        sub_response = requests.get(url.get('href'))
-        sub_soup = BeautifulSoup(sub_response.text, 'lxml')
-        tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e2o6ii40'})
-        
-        tag_list = []
+    sub_links = [url.get('href') for url in urls]
+    sub_reqs = (grequests.get(sub_link) for sub_link in sub_links)    
+    sub_resps =grequests.imap(sub_reqs,grequests.Pool(10))    
+    
+    tag_list = []
+    for sub_resp in sub_resps:
+        sub_soup = BeautifulSoup(sub_resp.text, 'lxml')
+        tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e2o6ii40'}) 
         for tag in tags:
             tag_list.append(tag.find('a').getText())
 
-    print(f"第{page}頁")
+    print(f"第{index+1}頁")
     print(title_list)
     print(tag_list)
+
+end_time = time.time()
+
+
+print(f"花費{end_time - start_time}秒")
