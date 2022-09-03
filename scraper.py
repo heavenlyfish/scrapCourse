d9b7c8c3a8df74357e25dbbc440f834d7539e400
heavenlyfish <42778096+heavenlyfish@users.noreply.github.com>
2022-09-03 17:48:38 +0800

    3.4 批次爬取子網頁的資料

--- scraper.py
+++ scraper.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.bbc.com/news/world')
+response = requests.get('https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt')
 soup = BeautifulSoup(response.text, 'lxml')
-titles = soup.find_all('a',{'class':'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'})
+titles = soup.find_all('h2',{'class':'bbc-19hmebw e47bds20'})
 
-title_list =[]
+title_list=[]
 for title in titles:
     title_list.append(title.getText())
-
-print(title_list)
+urls = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
+for url in urls:
+    sub_response = requests.get(url.get('href'))
+    sub_soup = BeautifulSoup(sub_response.text, 'lxml')
+    tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e2o6ii40'})
+    
+    tag_list = []
+    for tag in tags:
+        tag_list.append(tag.find('a').getText())
